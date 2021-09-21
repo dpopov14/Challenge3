64ecb37f716392a72b54c2a713b37157f650489f
dpopov14 <d.popov@student.utwente.nl>
2021-09-21 15:38:05 +0300

    Implemented reading Accelerometer and Gyroscope data; Still not polished but a good start

--- app/src/main/java/com/example/challenge3/MainActivity.java
+++ app/src/main/java/com/example/challenge3/MainActivity.java
@@ -1,14 +1,129 @@
 package com.example.challenge3;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensorAccelerometer;
+    private Sensor mSensorGyroscope;
+
+    List<float[]> AccelerometerData = new ArrayList<float[]>();
+    List<float[]> GyroscopeData = new ArrayList<float[]>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Button button;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get instance of a sensor manager
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        //Get a list of all sensors
+        List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        StringBuilder sensorText = new StringBuilder();
+
+        for (Sensor currentSensor : sensorList ) {
+            sensorText.append(currentSensor.getName()).append(
+                    System.getProperty("line.separator"));
+        }
+        //Print all sensor data
+        System.out.println(sensorText);
+
+
+
+        mSensorAccelerometer =
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        //Get the error string:
+        String sensor_error = getResources().getString(R.string.error_no_sensor);
+
+        button = findViewById(R.id.start_recording);
+        button.setOnClickListener(v -> {
+            this.onStart();
+        });
+
+        button = findViewById(R.id.stop_recording);
+        button.setOnClickListener(v -> {
+            this.onStop();
+        });
+
+
+
+
 //        first commit
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mSensorAccelerometer != null) {
+            mSensorManager.registerListener(this, mSensorAccelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorGyroscope != null) {
+            mSensorManager.registerListener(this, mSensorGyroscope,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
+
+        System.out.println("KUUUUUUUUUUUR");
+//        System.out.println(AccelerometerData.toString());
+
+        for(float[] a : AccelerometerData){
+            System.out.println("Row: " + a[0]+ " "+ a[1]+ " " + a[2]);
+        }
+
+
+//        System.out.println(GyroscopeData.toString());
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        int sensorType = sensorEvent.sensor.getType();
+
+        //Collect current x,y,z data
+        float[] currentData = sensorEvent.values;
+        switch(sensorType){
+            //Event came from the Accelerometer
+            case Sensor.TYPE_ACCELEROMETER:
+                //Handle Accelerometer
+                    AccelerometerData.add(currentData);
+                break;
+            case Sensor.TYPE_GYROSCOPE:
+                    //Handle Gyroscope
+                    GyroscopeData.add(currentData);
+                break;
+            default:
+                //do nothing;
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
