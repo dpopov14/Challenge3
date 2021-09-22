e37464cf3efd530fcada074c69baa236181ecba3
dpopov14 <d.popov@student.utwente.nl>
2021-09-22 14:18:26 +0300

    Pushvam tejko

--- app/src/main/java/com/example/challenge3/MainActivity.java
+++ app/src/main/java/com/example/challenge3/MainActivity.java
@@ -32,10 +32,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor mSensorLinearAccelerometer;
     private Sensor mSensorMagnetometer;
     private Random mRandom = new Random();
-    List<float[]> AccelerometerData = new ArrayList<float[]>();
-    List<float[]> GyroscopeData = new ArrayList<float[]>();
-    List<float[]> LinearAccelerometerData = new ArrayList<float[]>();
-    List<float[]> MagnetometerData = new ArrayList<float[]>();
+//    List<float[]> AccelerometerData = new ArrayList<float[]>();
+//    List<float[]> GyroscopeData = new ArrayList<float[]>();
+//    List<float[]> LinearAccelerometerData = new ArrayList<float[]>();
+//    List<float[]> MagnetometerData = new ArrayList<float[]>();
+    Double[] AccelerometerData = new Double[3];
+    Double[] GyroscopeData = new Double[3];
+    Double[] LinearAccelerometerData = new Double[3];
+    Double[] MagnetometerData = new Double[3];
     private List<double[]> sensorData  = new ArrayList<>();
 
 
@@ -167,6 +171,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Thread ActivityClassifierThread = new Thread(new Runnable() {
                 public void run()
                 {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     //will periodically check the list with samples and create an instance
                     Instances dataUnpredicted = new Instances("CurrentInstance",
                             attributeList, 1);
@@ -177,18 +186,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                     DenseInstance newInstance = new DenseInstance(dataUnpredicted.numAttributes()) {
                         {
-                            setValue(attributeAccelx, sensorData.get(sensorData.size()-1)[0]);
-                            setValue(attributeAccely, sensorData.get(sensorData.size()-1)[1]);
-                            setValue(attributeAccelz, sensorData.get(sensorData.size()-1)[2]);
-                            setValue(attributeLinx, sensorData.get(sensorData.size()-1)[3]);
-                            setValue(attributeLiny, sensorData.get(sensorData.size()-1)[4]);
-                            setValue(attributeLinz, sensorData.get(sensorData.size()-1)[5]);
-                            setValue(attributeGyrox, sensorData.get(sensorData.size()-1)[6]);
-                            setValue(attributeGyroy, sensorData.get(sensorData.size()-1)[7]);
-                            setValue(attributeGyroz, sensorData.get(sensorData.size()-1)[8]);
-                            setValue(attributeMagx, sensorData.get(sensorData.size()-1)[9]);
-                            setValue(attributeMagy, sensorData.get(sensorData.size()-1)[10]);
-                            setValue(attributeMagz, sensorData.get(sensorData.size()-1)[11]);
+                            setValue(attributeAccelx, AccelerometerData[0]);
+                            setValue(attributeAccely, AccelerometerData[1]);
+                            setValue(attributeAccelz, AccelerometerData[2]);
+                            setValue(attributeLinx, GyroscopeData[0]);
+                            setValue(attributeLiny, GyroscopeData[1]);
+                            setValue(attributeLinz, GyroscopeData[2]);
+                            setValue(attributeGyrox, LinearAccelerometerData[0]);
+                            setValue(attributeGyroy, LinearAccelerometerData[1]);
+                            setValue(attributeGyroz, LinearAccelerometerData[2]);
+                            setValue(attributeMagx, MagnetometerData[0]);
+                            setValue(attributeMagy, MagnetometerData[1]);
+                            setValue(attributeMagz, MagnetometerData[2]);
+//                            setValue(attributeAccelx, sensorData.get(sensorData.size()-1)[0]);
+//                            setValue(attributeAccely, sensorData.get(sensorData.size()-1)[1]);
+//                            setValue(attributeAccelz, sensorData.get(sensorData.size()-1)[2]);
+//                            setValue(attributeLinx, sensorData.get(sensorData.size()-1)[3]);
+//                            setValue(attributeLiny, sensorData.get(sensorData.size()-1)[4]);
+//                            setValue(attributeLinz, sensorData.get(sensorData.size()-1)[5]);
+//                            setValue(attributeGyrox, sensorData.get(sensorData.size()-1)[6]);
+//                            setValue(attributeGyroy, sensorData.get(sensorData.size()-1)[7]);
+//                            setValue(attributeGyroz, sensorData.get(sensorData.size()-1)[8]);
+//                            setValue(attributeMagx, sensorData.get(sensorData.size()-1)[9]);
+//                            setValue(attributeMagy, sensorData.get(sensorData.size()-1)[10]);
+//                            setValue(attributeMagz, sensorData.get(sensorData.size()-1)[11]);
+
                         }
                     };
 
@@ -219,9 +241,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         button.setOnClickListener(v -> {
             mSensorManager.unregisterListener(this);
             System.out.println("Sensors stopped");
-            for(float[] a : MagnetometerData){
-                System.out.println("Row: " + a[0]+ " "+ a[1]+ " " + a[2]);
-            }
+//            for(float[] a : MagnetometerData){
+////                System.out.println("Row: " + a[0]+ " "+ a[1]+ " " + a[2]);
+////            }
+
+            System.out.println("AccData: " + AccelerometerData.toString());
+            System.out.println("GyroData: " + GyroscopeData.toString());
+            System.out.println("LinearData: " + LinearAccelerometerData.toString());
+            System.out.println("MagnetData: " + MagnetometerData.toString());
 
         });
     }
@@ -230,25 +257,34 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         int sensorType = sensorEvent.sensor.getType();
 
         //Collect current x,y,z data
-        float[] currentData = sensorEvent.values;
+        Double[] currentData = new Double[3];
+        for(int i = 0;  i < 3; i++){
+            currentData[i] = Double.parseDouble(new Float(sensorEvent.values[i]).toString());
+        }
+//        Double[] currentData = Double.parseDouble(sensorEvent.values.toString());
 
         switch(sensorType){
             //Event came from the Accelerometer
             case Sensor.TYPE_ACCELEROMETER:
+                AccelerometerData = currentData;
                 //Handle Accelerometer
-                    AccelerometerData.add(currentData);
+//                    AccelerometerData.add(currentData);
                 break;
             case Sensor.TYPE_GYROSCOPE:
                     //Handle Gyroscope
-                    GyroscopeData.add(currentData);
+//                    GyroscopeData.add(currentData);
+                    GyroscopeData = currentData;
+
                 break;
             case Sensor.TYPE_LINEAR_ACCELERATION:
                 //Handle Linear Accelerometer
-                    LinearAccelerometerData.add(currentData);
-                    break;
+//                    LinearAccelerometerData.add(currentData);
+                LinearAccelerometerData = currentData;
+                break;
             case Sensor.TYPE_MAGNETIC_FIELD:
                 //Handle Magnetometer
-                MagnetometerData.add(currentData);
+//                MagnetometerData.add(currentData);
+                MagnetometerData = currentData;
                 break;
             default:
                 //do nothing;
